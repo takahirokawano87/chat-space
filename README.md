a83200fc056e0e83b62383fb5298103def6ff9fe
takahirokawano87 <takahirokawano@kounotakahironoMacBook-ea.local>
2018-11-20 15:35:22 +0900

    readme_haml_scss_update_1120_1535

--- README.md
+++ README.md
@@ -53,7 +53,7 @@ Things you may want to cover:
 
 |Column|Type|Options|
 |------|----|-------|
-|name|string|null: false, add_index: true|
+|name|string|null: false, index: true|
 |email|string|null: false, unique: true|
 |password|string|null: false|
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- root "message#index"
+  devise_for :users
+  root 'message#index'
+  resources :users, only: [:edit, :update]
 end
-
 
