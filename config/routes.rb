009bd2358c030f2ae89273f48e52d784097f69bf
takahirokawano87 <takahirokawano@kounotakahironoMacBook-ea.local>
2018-11-27 09:45:35 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'groups#index'
-  resources :users, only: [:index, :edit, :update]
+  devise_for :users
+  resources :users, only: [:index, :edit, :update, :search]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
 end
-
